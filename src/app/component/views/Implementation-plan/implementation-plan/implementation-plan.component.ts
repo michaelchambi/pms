56eed2aa4f2add1ec0309de4a70f0005d8ddf389
Jicabea <beatricejika12@gmail.com>
2022-11-03 13:39:38 +0300

    implementation form is added

--- src/app/component/views/Implementation-plan/implementation-plan/implementation-plan.component.ts
+++ src/app/component/views/Implementation-plan/implementation-plan/implementation-plan.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GeneralService } from 'src/app/service/general/general.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-implementation-plan',
   templateUrl: './implementation-plan.component.html',
   styleUrls: ['./implementation-plan.component.css']
 })
 export class ImplementationPlanComponent implements OnInit {
-
-  constructor() { }
+  token: any;
+  sub_moduleId: any;
+  constructor(
+    private general: GeneralService,
+    private activeRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+    this.general.verifyToken();
+    this.sub_moduleId = this.activeRoute.snapshot.paramMap.get('id');
   }
 
 }
